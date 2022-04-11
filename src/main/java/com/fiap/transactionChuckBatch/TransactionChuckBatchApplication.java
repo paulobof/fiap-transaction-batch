18a1ccb92cd3fe325c5fd5afda2f0411db7078a0
Paulo Bof <paulobof@gmail.com>
2022-04-11 19:05:40 -0300

    Criando Spring Batch MongoDB

---
+++ src/main/java/com/fiap/transactionChuckBatch/TransactionChuckBatchApplication.java
@@ -0,0 +1,83 @@
+package com.fiap.transactionChuckBatch;
+
+import com.fiap.transactionChuckBatch.domain.User;
+import com.fiap.transactionChuckBatch.listener.JobCompletionNotificationListener;
+import com.fiap.transactionChuckBatch.model.UserDetail;
+import com.fiap.transactionChuckBatch.processor.UserItemProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.data.MongoItemWriter;
+import org.springframework.batch.item.data.builder.MongoItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableBatchProcessing
+public class TransactionChuckBatchApplication {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	private Logger logger = Logger.getLogger(TransactionChuckBatchApplication.class.getSimpleName());
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionChuckBatchApplication.class, args);
+	}
+
+	@Bean
+	public FlatFileItemReader<UserDetail> reader() {
+		return new FlatFileItemReaderBuilder<UserDetail>().name("userItemReader")
+				.resource(new ClassPathResource("user-sample-data.csv")).delimited()
+				.names(new String[] {"email", "firstName", "lastName", "mobileNumber"})
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<UserDetail>() {
+					{
+						setTargetType(UserDetail.class);
+					}
+				}).build();
+	}
+
+	@Bean
+	public UserItemProcessor processor() {
+		return new UserItemProcessor();
+	}
+
+
+	@Bean
+	public Step step1(FlatFileItemReader<UserDetail> itemReader, MongoItemWriter<User> itemWriter)
+			throws Exception {
+
+		return this.stepBuilderFactory.get("step1").<UserDetail, User>chunk(5).reader(itemReader)
+				.processor(processor()).writer(itemWriter).build();
+	}
+
+	@Bean
+	public Job updateUserJob(JobCompletionNotificationListener listener, Step step1)
+			throws Exception {
+
+		return this.jobBuilderFactory.get("updateUserJob").incrementer(new RunIdIncrementer())
+				.listener(listener).start(step1).build();
+	}
+
+	@Bean
+	public MongoItemWriter<User> writer(MongoTemplate mongoTemplate) {
+		return new MongoItemWriterBuilder<User>().template(mongoTemplate).collection("user")
+				.build();
+	}
+}
